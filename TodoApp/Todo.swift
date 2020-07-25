14e8f76e455bdbc17967f66c092686cc38b89265
Rejke <Rejke@yandex.ru>
2020-07-25 12:35:54 +0300

    Added Todo model

---
+++ TodoApp/Todo.swift
@@ -0,0 +1,24 @@
+//
+//  Todo.swift
+//  TodoApp
+//
+//  Created by Valery Smirnov on 25/07/2020.
+//  Copyright © 2020 Valery Smirnov. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Todo: JSONableProtocol {
+    var id: Int
+    var text: String
+    var isCompleted: Bool
+    var projectId: Int
+    
+    required init(parameter: JSON) {
+        id = parameter["id"].intValue
+        text = parameter["title"].stringValue
+        isCompleted = parameter["is_completed"].boolValue
+        projectId = parameter["project_id"].intValue
+    }
+}
